bfa96b18bd282d71a66e1c8f5a20d38bd248b967
Luis Tome <luisptcolaco@gmail.com>
2020-10-13 12:48:06 +0100

    Initial Check-in

---
+++ oh-my-gacha/src/main/java/com/ohmygacha/OhMyGachaApplication.java
@@ -0,0 +1,13 @@
+package com.ohmygacha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OhMyGachaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OhMyGachaApplication.class, args);
+	}
+
+}

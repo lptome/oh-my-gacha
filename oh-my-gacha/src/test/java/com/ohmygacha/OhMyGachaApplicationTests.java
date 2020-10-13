0e91f13ee9c32f5c9690d03a0a056ba64f06c3d4
Luis Tome <luisptcolaco@gmail.com>
2020-10-13 21:52:18 +0100

    Calculator Class and JUnit tests for numberOfTrials() method.

--- oh-my-gacha/src/test/java/com/ohmygacha/OhMyGachaApplicationTests.java
+++ oh-my-gacha/src/test/java/com/ohmygacha/OhMyGachaApplicationTests.java
@@ -1,5 +1,7 @@
 package com.ohmygacha;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,7 +9,15 @@ import org.springframework.boot.test.context.SpringBootTest;
 class OhMyGachaApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void numberOfTrialsTest() {
+		var calc = new Calculator();
+		assertEquals(225, calc.numberOfTrials(0.05), "For 0.05 probability of a,"
+				+ "p of a occuring at least once approaches 1 at 225 trials");
+		assertEquals(83, calc.numberOfTrials(0.13), "For 0.13 probability of a,"
+				+ "p of a occuring at least once approaches 1 at 83 trials");
+		assertEquals(1, calc.numberOfTrials(1.0), "For 1.0 probability of a,"
+				+ "p of a occuring at least once approaches 1 after 1 trial.");
+		
 	}
 
 }

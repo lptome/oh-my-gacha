2f72e33bca34e9325379826df5f56edb818c860a
Luis Tome <luisptcolaco@gmail.com>
2020-10-17 16:09:01 +0100

    - Change Calculator class to use Integer and BigDecimal - Update Unit  tests to reflect this change

--- oh-my-gacha/src/main/java/com/ohmygacha/domain/Calculator.java
+++ oh-my-gacha/src/main/java/com/ohmygacha/domain/Calculator.java
@@ -1,59 +1,63 @@
 package com.ohmygacha.domain;
 
+import java.math.BigDecimal;
+
 public class Calculator {
 	
-	private double probability;
-	private int pullCost;
-	private double moneyIn;
-	private int currencyOut;
-	private int attempts;
-	private int totalCost;
+	//@DecimalMin(value="0.0", inclusive = false)
+	private BigDecimal probability;	
+	private Integer pullCost;
+	private BigDecimal moneyIn;
+	private Integer currencyOut;
+	private Integer attempts;
+	private Integer totalCost;
 	
-	public double getProbability() {
+	
+	public BigDecimal getProbability() {
 		return probability;
 	}
 
-	public void setProbability(double probability) {
+	public void setProbability(BigDecimal probability) {
 		this.probability = probability;
 	}
 
-	public int getAttempts() {
-		return attempts;
-	}
-
-	public void setAttempts(int attempts) {
-		this.attempts = attempts;
-	}
-
-	public double getMoneyIn() {
-		return moneyIn;
-	}
-
-	public void setMoneyIn(double moneyIn) {
-		this.moneyIn = moneyIn;
-	}
-
-	public int getCurrencyOut() {
-		return currencyOut;
-	}
-
-	public void setCurrencyOut(int currencyOut) {
-		this.currencyOut = currencyOut;
-	}
-
-	public int getPullCost() {
+	public Integer getPullCost() {
 		return pullCost;
 	}
 
-	public void setPullCost(int pullCost) {
+	public void setPullCost(Integer pullCost) {
 		this.pullCost = pullCost;
 	}
 
-	public int getTotalCost() {
+	public BigDecimal getMoneyIn() {
+		return moneyIn;
+	}
+
+	public void setMoneyIn(BigDecimal moneyIn) {
+		this.moneyIn = moneyIn;
+	}
+
+	public Integer getCurrencyOut() {
+		return currencyOut;
+	}
+
+	public void setCurrencyOut(Integer currencyOut) {
+		this.currencyOut = currencyOut;
+	}
+
+	public Integer getAttempts() {
+		return attempts;
+	}
+
+	public void setAttempts(Integer attempts) {
+		this.attempts = attempts;
+	}
+
+	public Integer getTotalCost() {
 		return totalCost;
 	}
 
-	public void setTotalCost(int totalCost) {
+	public void setTotalCost(Integer totalCost) {
 		this.totalCost = totalCost;
 	}
 
@@ -62,20 +66,23 @@ public class Calculator {
 	 * This is calculated using logarithms, a threshold is chosen after which we
 	 * consider 0.9999 to be close enough to 1.
 	**/
-	public int numberOfTrials(double probability) {
+	public int numberOfTrials(BigDecimal probability) {
 		int attempts;
 		double threshold = 0.00001;
-		if (probability == 1) {	//log(0) is undefined so return 1 if probability is 1
+		double temp = probability.doubleValue();
+		
+		if (temp == 1) {	//log(0) is undefined so return 1 if probability is 1
 			return 1;
 		}
 		// Change of base formula --> logb(x) = loga(x) / loga(b)
-		attempts = (int) Math.ceil(Math.log10(threshold) / Math.log10(1 - probability));
+		attempts = (int) Math.ceil(Math.log10(threshold) / Math.log10(1 - temp));
 		return attempts;
 	}
 	
 	//Computes the expected minimum cost of obtaining a specific loot drop.
-	public int totalCost(int attempts, double moneyIn, int currencyOut, int pullCost) {
-		double conversionRate = moneyIn / currencyOut;
+	public int totalCost(int attempts, BigDecimal moneyIn, Integer currencyOut, Integer pullCost) {
+		double moneyInTemp = moneyIn.doubleValue();
+		double conversionRate = moneyInTemp / currencyOut;
 		double totalCost = Math.ceil(conversionRate * attempts * pullCost);
 		return (int) totalCost;
 	}
--- oh-my-gacha/src/test/java/com/ohmygacha/OhMyGachaApplicationTests.java
+++ oh-my-gacha/src/test/java/com/ohmygacha/OhMyGachaApplicationTests.java
@@ -2,6 +2,8 @@ package com.ohmygacha;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.math.BigDecimal;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -13,11 +15,11 @@ class OhMyGachaApplicationTests {
 	@Test
 	void numberOfTrialsTest() {
 		var calc = new Calculator();
-		assertEquals(225, calc.numberOfTrials(0.05),
+		assertEquals(225, calc.numberOfTrials(new BigDecimal("0.05")),
 				"For 0.05 probability of a," + "p of a occuring at least once approaches 1 at 225 trials");
-		assertEquals(83, calc.numberOfTrials(0.13),
+		assertEquals(83, calc.numberOfTrials(new BigDecimal("0.13")),
 				"For 0.13 probability of a," + "p of a occuring at least once approaches 1 at 83 trials");
-		assertEquals(1, calc.numberOfTrials(1.0),
+		assertEquals(1, calc.numberOfTrials(new BigDecimal("1.0")),
 				"For 1.0 probability of a," + "p of a occuring at least once approaches 1 after 1 trial.");
 
 	}
@@ -25,9 +27,9 @@ class OhMyGachaApplicationTests {
 	@Test
 	void totalCostTest() {
 		var calc = new Calculator();
-		assertEquals(1000.0, calc.totalCost(200, 99.99, 2000, 100), "200 attempts should cost £1000.00");
-		assertEquals(750.0, calc.totalCost(500, 38.99, 13000, 500), "500 attempts should cost £750.00");
-		assertEquals(23.0, calc.totalCost(20, 8.99, 2000, 250), "20 attempts should cost £23.00");
+		assertEquals(1000.0, calc.totalCost(200, new BigDecimal("99.99"), 2000, 100), "200 attempts should cost £1000.00");
+		assertEquals(750.0, calc.totalCost(500, new BigDecimal("38.99"), 13000, 500), "500 attempts should cost £750.00");
+		assertEquals(23.0, calc.totalCost(20, new BigDecimal("8.99"), 2000, 250), "20 attempts should cost £23.00");
 		
 	}
 
